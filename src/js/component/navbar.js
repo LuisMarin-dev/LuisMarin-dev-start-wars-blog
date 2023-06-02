bbe52af1e3ac6eed0b4a808b630157a3deed11d8
LuisMarin-dev <luis.marinprog@gmail.com>
2023-06-02 16:23:24 -0400

    created the character and planets cards, fetched the api data and added the simple dropdown

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,20 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">My Star Wars Blog</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<div className="btn-group dropstart">
+					<button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites
+					</button>
+					<ul className="dropdown-menu">
+						<li><a className="dropdown-item" href="#">Menu item</a></li>
+						<li><a className="dropdown-item" href="#">Menu item</a></li>
+						<li><a className="dropdown-item" href="#">Menu item</a></li>
+					</ul>
+				</div>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useContext} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const {store, actions} = useContext(Context);
+
+	return(
+		<div className="text-center mt-5">
+			<div className="people-carousel d-flex" >
+				{store.people.map((character) => {
+					return(
+						<div className="card" style={{minWidth: "18rem"}}>
+						<img src={`https://starwars-visualguide.com/assets/img/characters/${character.uid}.jpg`} className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">{character.properties.name}</h5>
+							<p className="card-text">Gender: {character.properties.gender}</p>
+							<p className="card-text">Hair-color: {character.properties.hair_color}</p>
+							<p className="card-text">Eye-color: {character.properties.eye_color}</p>
+							<a href="#" className="btn btn-outline-primary ">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning "><i class="far fa-heart"></i></a>
+						</div>
+						</div>
+					)
+				})}
+			</div>
+			<div className="planet-carousel d-flex">
+				{store.planets.map((planet) => {
+					return(
+						<div className="card" style={{minWidth: "18rem"}}>
+						<img src={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`} className="card-img-top" alt="..." />
+						<div className="card-body">
+							<h5 className="card-title">{planet.properties.name}</h5>
+							<p className="card-title">Population: {planet.properties.population}</p>
+							<p className="card-text">Terrain: {planet.properties.terrain}</p>
+							<a href="#" className="btn btn-outline-primary">Learn more!</a>
+							<a href="#" className="btn btn-outline-warning "><i class="far fa-heart"></i></a>
+						</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>)
+};
